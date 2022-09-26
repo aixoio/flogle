af4cfc8ca951b54e34fb83afd1f157bb2bdbd5d3
aixoio <aixoio@yahoo.com>
2022-09-26 21:11:57 +1000

    Base for docs

---
+++ docs/index.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/libs/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/docs.css">
+    <script src="../js/libs/bootstrap.bundle.min.js"></script>
+    <script src="../js/libs/jquery-3.6.1.min.js"></script>
+    <script src="../js/uuid_gen.js"></script>
+    <script src="../js/docs.js"></script>
+    <title>Flogle - Docs</title>
+</head>
+
+<body>
+    <span id="canStay" hidden><?php echo $_SESSION["safe"]; ?></span>
+    <h1>Docs</h1>
+    <button class="btn btn-"></button>
+    <div class="docsbox"></div>
+</body>
+
+</html>
