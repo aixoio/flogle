4348e4ef3a77e2215f3e9c35cdeeb9b0c41668a2
aixoio <aixoio@yahoo.com>
2022-09-27 11:03:08 +1000

    Move files

--- docs/index.php
+++ docs/index.php
@@ -9,11 +9,11 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/libs/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/docs.css">
+    <link rel="stylesheet" href="css/docs.css">
     <script src="../js/libs/bootstrap.bundle.min.js"></script>
     <script src="../js/libs/jquery-3.6.1.min.js"></script>
     <script src="../js/uuid_gen.js"></script>
-    <script src="../js/docs.js"></script>
+    <script src="js/docs.js"></script>
     <title>Flogle - Docs</title>
 </head>
 
