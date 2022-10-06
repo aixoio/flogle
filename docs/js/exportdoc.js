68346e97be8a16e1bf2e9823705b116437197e50
aixoio <aixoio@yahoo.com>
2022-10-06 21:14:20 +1100

    Bug fix

--- docs/js/exportdoc.js
+++ docs/js/exportdoc.js
@@ -51,7 +51,7 @@ $(window).on("load", async function () {
             <meta charset="UTF-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Flogle - Docs - Edit - ${docData[0].title}</title>
+            <title>Flogle - View - Edit - ${docData[0].title}</title>
         </head>
 
         <body>
