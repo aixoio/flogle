c703fe468965c56fe5713504e199256bb7a8aa29
aixoio <aixoio@yahoo.com>
2022-10-18 19:01:02 +1100

    Add fCaptcha to login and signup

--- js/login.js
+++ js/login.js
@@ -1,49 +1,116 @@
 $(window).on("load", async function() {
 
+    let canLogin = false;
+
     $(".loading").hide();
+
+    textfCaptcha(".captcha", () => {
+
+        canLogin = true;
+        $(".captcha").empty();
+
+    }, null, null, null, false, "Enter the text you see to login", "center")
 
     $("#cUser").on("click", async function() {
 
-        let usernameE = $("#usernameE");
-        let passwordE = $("#passwordE");
+        if (canLogin) {
 
-        let user = {
+            $(".loading").show();
+            $(".form").hide();
 
-            username: usernameE.val(),
-            password_hash: sha256(passwordE.val())
+            let usernameE = $("#usernameE");
+            let passwordE = $("#passwordE");
 
-        };
+            let user = {
 
-        if (user.username == "") return;
-        if (passwordE.val() == "") return;
+                username: usernameE.val(),
+                password_hash: sha256(passwordE.val())
 
-        let userFromDB = await ajax("php/getuserbyusername.php", {
+            };
 
-            username: user.username
+            if (user.username == "") {
 
-        }, "POST", "json");
+                $(".loading").hide();
+                $(".captcha").empty();
+                $(".form").show();
+                textfCaptcha(".captcha", () => {
 
-        if (userFromDB.length <= 0) {
+                    canLogin = true;
+                    $(".captcha").empty();
 
-            return;
+                }, null, null, null, false, "Enter the text you see to login", "center")
+                return;
+
+            }
+            if (passwordE.val() == "") {
+
+                $(".loading").hide();
+                $(".captcha").empty();
+                $(".form").show();
+                textfCaptcha(".captcha", () => {
+
+                    canLogin = true;
+                    $(".captcha").empty();
+
+                }, null, null, null, false, "Enter the text you see to login", "center")
+                return;
+
+            }
+
+            let userFromDB = await ajax("php/getuserbyusername.php", {
+
+                username: user.username
+
+            }, "POST", "json");
+
+            if (userFromDB.length <= 0) {
+
+                $(".loading").hide();
+                $(".captcha").empty();
+                $(".form").show();
+                textfCaptcha(".captcha", () => {
+
+                    canLogin = true;
+                    $(".captcha").empty();
+
+                }, null, null, null, false, "Enter the text you see to login", "center")
+                return;
+
+            }
+
+            if (userFromDB[0].password_hash != user.password_hash) {
+
+                $(".loading").hide();
+                $(".captcha").empty();
+                $(".form").show();
+                textfCaptcha(".captcha", () => {
+
+                    canLogin = true;
+                    $(".captcha").empty();
+
+                }, null, null, null, false, "Enter the text you see to login", "center")
+                return;
+
+            }
+
+            let isNowLogedIn = await ajax("php/loginaction.php", {
+
+                username: user.username
+
+            }, "POST", "json");
+
+
+            if (isNowLogedIn == true) {
+
+                location.href = "../dash.php";
+
+            }
+
+        } else {
+
+            alert("You have to verify you are a human")
 
         }
-
-        if (userFromDB[0].password_hash != user.password_hash) return;
-
-        let isNowLogedIn = await ajax("php/loginaction.php", {
-
-            username: user.username
-
-        }, "POST", "json");
-
-
-        if (isNowLogedIn == true) {
-
-            location.href = "../dash.php";
-
-        }
-
         
 
     })
--- js/signup.js
+++ js/signup.js
@@ -1,59 +1,108 @@
 $(window).on("load", async function() {
 
     $(".loading").hide();
+    
+    let canSignup = false;
+
+    textfCaptcha(".captcha", () => {
+
+        canSignup = true;
+        $(".captcha").empty();
+
+    }, null, null, null, true, "Enter the text you see to signup", "center")
 
     $("#cUser").on("click", async function() {
 
-        $(".form").hide();
-        $(".loading").show();
 
-        let usernameE = $("#usernameE");
-        let passwordE = $("#passwordE");
+        if (canSignup) {
 
-        let user = {
+            $(".form").hide();
+            $(".loading").show();
 
-            username: usernameE.val(),
-            password_hash: sha256(passwordE.val())
+            let usernameE = $("#usernameE");
+            let passwordE = $("#passwordE");
 
-        }
+            let user = {
 
-        if (user.username == "") return;
+                username: usernameE.val(),
+                password_hash: sha256(passwordE.val())
 
-        if (passwordE.val() == "") return;
+            }
 
-        let doesThatUser = await ajax("php/getuserbyusername.php", {
+            if (user.username == "") {
 
-            username: user.username
+                $(".form").show();
+                $(".loading").hide();
+                $(".captcha").empty();
+                textfCaptcha(".captcha", () => {
 
-        }, "POST", "json");
+                    canSignup = true;
+                    $(".captcha").empty();
 
-        if (doesThatUser.length >= 1) {
+                }, null, null, null, true, "Enter the text you see to signup", "center")
+                return;
 
-            $(".form").show();
-            $(".loading").hide();
-            return;
+            }
 
-        }
+            if (passwordE.val() == "") {
 
-        let made = await ajax("php/newuser.php", {
+                $(".form").show();
+                $(".loading").hide();
+                $(".captcha").empty();
+                textfCaptcha(".captcha", () => {
 
-            username: user.username,
-            password_hash: user.password_hash
+                    canSignup = true;
+                    $(".captcha").empty();
 
-        }, "POST", "json");
+                }, null, null, null, true, "Enter the text you see to signup", "center")
+                return;
 
-        if (made[0] == true) {
+            }
 
-            location.href = "../dash.php";
+            let doesThatUser = await ajax("php/getuserbyusername.php", {
+
+                username: user.username
+
+            }, "POST", "json");
+
+            if (doesThatUser.length >= 1) {
+
+                $(".form").show();
+                $(".loading").hide();
+                $(".captcha").empty();
+                textfCaptcha(".captcha", () => {
+
+                    canSignup = true;
+                    $(".captcha").empty();
+
+                }, null, null, null, true, "Enter the text you see to signup", "center")
+                return;
+
+            }
+
+            let made = await ajax("php/newuser.php", {
+
+                username: user.username,
+                password_hash: user.password_hash
+
+            }, "POST", "json");
+
+            if (made[0] == true) {
+
+                location.href = "../dash.php";
+
+            } else {
+
+                alert("Error");
+                location.reload();
+
+            }
 
         } else {
 
-            alert("Error");
-            location.reload();
+            alert("You have to verify you are a human")
 
         }
-
-
 
     })
 
