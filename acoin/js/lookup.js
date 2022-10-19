0462a99d473136b15a9bd825e9bd8cc1df878a2d
aixoio <aixoio@yahoo.com>
2022-10-19 17:24:26 +1100

    Add fCaptcha to lookup

--- acoin/js/lookup.js
+++ acoin/js/lookup.js
@@ -1,45 +1,59 @@
 $(window).on("load", function () {
 
     $(".resultData").hide();
+    $(".captcha").hide();
     
     $("#lookupUser").on("click", async function () {
         
         $(".askForUser").hide();
 
-        let usernameIn = $("#userNameIn").val();
+        $(".captcha").show();
 
-        let userData = await ajax("../../php/getuserbyusername.php", {
+        textfCaptcha(".captcha", async () => {
 
-            username: usernameIn
+            $(".captcha").empty();
+            $(".captcha").hide();
 
-        }, "POST", "json");
+            let usernameIn = $("#userNameIn").val();
 
-        if (userData.length <= 0) {
+            let userData = await ajax("../../php/getuserbyusername.php", {
 
-            $(".askForUser").show();
-            return;
+                username: usernameIn
 
-        }
+            }, "POST", "json");
 
-        let acoinData = await ajax("../../php/getacoininfobyusername.php", {
+            if (userData.length <= 0) {
 
-            username: userData[0].username
+                $(".askForUser").show();
+                $(".captcha").empty();
+                $(".captcha").hide();
+                return;
 
-        }, "POST", "json");
+            }
 
-        if (acoinData.length <= 0) {
+            let acoinData = await ajax("../../php/getacoininfobyusername.php", {
 
-            $(".askForUser").show();
-            return;
+                username: userData[0].username
 
-        }
+            }, "POST", "json");
 
-        $(".resultData").show();
+            if (acoinData.length <= 0) {
 
-        $("#usernameR").text(`Username: ${userData[0].username}`);
-        $("#acoinsR").text(`aCoins: ${acoinData[0].acoins}`);
-        $("#lockedR").text(acoinData[0].locked == "1" ? "This user is locked" : "This user is unlocked");
-        $("#adminR").text(acoinData[0].admin == "1" ? "This user is an admin" : "This user is not an admin");
+                $(".askForUser").show();
+                $(".captcha").empty();
+                $(".captcha").hide();
+                return;
+
+            }
+
+            $(".resultData").show();
+
+            $("#usernameR").text(`Username: ${userData[0].username}`);
+            $("#acoinsR").text(`aCoins: ${acoinData[0].acoins}`);
+            $("#lockedR").text(acoinData[0].locked == "1" ? "This user is locked" : "This user is unlocked");
+            $("#adminR").text(acoinData[0].admin == "1" ? "This user is an admin" : "This user is not an admin");
+
+        }, null, null, null, true, "Enter the text you see to lookup that user", "center")
 
 
     })
