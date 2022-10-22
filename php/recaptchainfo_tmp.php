84191cd6d90e4c95a1fe019b28601a48ef87eb74
aixoio <aixoio@yahoo.com>
2022-10-22 17:23:29 +1100

    Add reCaptcha info

---
+++ php/recaptchainfo_tmp.php
@@ -0,0 +1,16 @@
+<?php
+
+    // Rename this file to recaptchainfo.php and replace the info with your info
+
+    function getreCaptchaSiteKeyCheckbox()
+    {
+        return "yourreCaptchaSiteKeyCheckbox";
+    }
+
+    function getreCaptchaSecretKeyCheckbox()
+    {
+        return "yourreCaptchaSecretKeyCheckbox";
+    }
+
+
+?>
