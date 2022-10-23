00b69190a16632a0ae98fa48cb1df4f75c462aee
aixoio <aixoio@yahoo.com>
2022-10-23 18:23:40 +1100

    Add reCaptcha v3 to aCoin user Lookup

--- acoin/js/lookup.js
+++ acoin/js/lookup.js
@@ -1,3 +1,8 @@
+$.getScript("https://www.google.com/recaptcha/api.js?onload=onloadreCaptchaCallback&render=" + getreCaptchaSiteKeyV3());
+
+let canLookup = false;
+let scoreVerifyed = false;
+let canScoreVerify = false;
 
 function onloadreCaptchaCallback() {
     
@@ -13,50 +18,90 @@ function onloadreCaptchaCallback() {
 
             }, "POST", "json");
 
-            if (reData.success) {
+            canLookup = true;
 
-                $(".askForUser").hide();
+            grecaptcha.ready(() => {
 
 
-                let usernameIn = $("#userNameIn").val();
+                grecaptcha.execute(getreCaptchaSiteKeyV3(), {
+                    action: "aCoin_User_Lookup"
+                }).then(async (token) => {
+                    
+                    let v3reData = await ajax("../php/recaptchaapi.php", {
 
-                let userData = await ajax("../../php/getuserbyusername.php", {
+                        mode: "v3",
+                        token: token
 
-                    username: usernameIn
+                    }, "POST", "json");
+                    
+                    if (v3reData.score == 0.5 || scoreVerifyed) {
+                        
+                        canLookup = true;
 
-                }, "POST", "json");
+                        if (reData.success && canLookup) {
+            
+                            $(".askForUser").hide();
+            
+            
+                            let usernameIn = $("#userNameIn").val();
+            
+                            let userData = await ajax("../../php/getuserbyusername.php", {
+            
+                                username: usernameIn
+            
+                            }, "POST", "json");
+            
+                            if (userData.length <= 0) {
+            
+                                $(".askForUser").show();
+                                grecaptcha.reset(recaptcha);
+                                return;
+            
+                            }
+            
+                            let acoinData = await ajax("../../php/getacoininfobyusername.php", {
+            
+                                username: userData[0].username
+            
+                            }, "POST", "json");
+            
+                            if (acoinData.length <= 0) {
+            
+                                $(".askForUser").show();
+                                grecaptcha.reset(recaptcha);
+                                return;
+            
+                            }
+            
+                            $(".resultData").show();
+            
+                            $("#usernameR").text(`Username: ${userData[0].username}`);
+                            $("#acoinsR").text(`aCoins: ${acoinData[0].acoins}`);
+                            $("#lockedR").text(acoinData[0].locked == "1" ? "This user is locked" : "This user is unlocked");
+                            $("#adminR").text(acoinData[0].admin == "1" ? "This user is an admin" : "This user is not an admin");
+            
+            
+                        }
+                        
+                    } else {
 
-                if (userData.length <= 0) {
+                        canLookup = false;
+                        canScoreVerify = true;
+                        grecaptcha.reset(recaptcha);
+                        grecaptcha.execute(recaptcha);
 
-                    $(".askForUser").show();
-                    grecaptcha.reset(recaptcha);
-                    return;
+                    }
 
-                }
+                })
 
-                let acoinData = await ajax("../../php/getacoininfobyusername.php", {
+            })
 
-                    username: userData[0].username
+            if (reData.success && canScoreVerify) {
 
-                }, "POST", "json");
-
-                if (acoinData.length <= 0) {
-
-                    $(".askForUser").show();
-                    grecaptcha.reset(recaptcha);
-                    return;
-
-                }
-
-                $(".resultData").show();
-
-                $("#usernameR").text(`Username: ${userData[0].username}`);
-                $("#acoinsR").text(`aCoins: ${acoinData[0].acoins}`);
-                $("#lockedR").text(acoinData[0].locked == "1" ? "This user is locked" : "This user is unlocked");
-                $("#adminR").text(acoinData[0].admin == "1" ? "This user is an admin" : "This user is not an admin");
-
+                scoreVerifyed = true;
 
             }
+
 
         }
 
